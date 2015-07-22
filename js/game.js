82d63dbc204021ed20a7d7f3846bcbd32b6902ff
aarongoa <taipan93@gmail.com>
2015-07-22 23:37:32 +0530

    Wrap around - rocket & asteroids moveFwd flag removed, acceleration introduced Reversing of rocket removed Code optimizations

--- js/game.js
+++ js/game.js
@@ -15,8 +15,7 @@ function loaded () {
 		speed: 0,
 		angle: 90,
 		move: false,
-		moveFwd: false,
-		moveBack: false,
+		accelerate: false,
 		rotating: false,
 		clkwise: true,
 		draw : null,
@@ -31,57 +30,75 @@ function loaded () {
 		this.sprite = null;
 		this.x = Math.random() * global.width;
 		this.y = Math.random() * global.height;
-		this.dim = Math.random();
-		this.width = 20 + this.dim * 50;
-		this.height = 30 + this.dim * 50;
+		this.dimension = Math.random();
+		this.width = 20 + this.dimension * 50;
+		this.height = 30 + this.dimension * 50;
 		this.speed = 0;
+		this.xmovement = Math.random() / 3;
+		this.ymovement = Math.random() / 3;
 		this.angle = 30;
 		this.rotating = false;
 		this.clkwise = true;
 	};
+
+	var Bullet = function() {
+		this.sprite = null;
+		this.x = null;
+		this.y = null;
+		this.width = 5 ;
+		this.height = 5 ;
+		this.speed = 0;
+	};
+
+	Bullet.prototype.draw = function() {
+
+	};
+
+	Bullet.prototype.update = function() {
+		
+	};
+
+	/*for collisions*/
+	var intersectRect = function(a, b) {
+		
+	}
 
 	Meteor.prototype.draw = function() {
 		context.drawImage(this.sprite, this.x, this.y, this.width, this.height);
 	};
 
 	Meteor.prototype.update = function() {
-		
-	};
 
-	rocket.draw = function() {
-		context.translate(rocket.x, rocket.y); /*Translate context to rocket position. ie, get it to rocket pos.*/
-		context.rotate(rocket.angle*(Math.PI/180)); /*Rotate context to angle required*/
-		
-		if (rocket.move) {
-			context.drawImage(rocket.spriteMoving, 0, 0, rocket.width, rocket.height+4); /*Draw the image onto rotated context*/
-		} else {
-			context.drawImage(rocket.spriteNormal, 0, 0, rocket.width, rocket.height); /*Draw the image onto rotated context*/
+		for (var i = 0; i < meteor.length; i++) {
+			
+			this.x += this.xmovement;
+			this.y += this.ymovement;
+
+			//to wrap the asteroids
+			if (this.x > global.width) {
+				this.x = 0;
+			} else if (this.x < 0) {
+				this.x = global.width;
+			} else if (this.y > global.height) {
+				this.y = 0;
+			} else if (this.y < 0) {
+				this.y = global.height;
+			};
 		};
-		
-		context.rotate(rocket.angle*(Math.PI/180) * -1); /*Rotate context back to normal after drawing*/
-		context.translate(-rocket.x, -rocket.y); /*Translate context back to old position*/
 	};
 
 	rocket.up = function(status) {
 		if (status == "pressed") {
 			rocket.move = true;
-			rocket.moveFwd = true;
-			rocket.moveBack = false;
-		} else{
-			rocket.move = false;
-			// rocket.moveFwd = false;
+			rocket.accelerate = true;
+		} else {
+			rocket.accelerate = false;
 		};
 	};
 
-	rocket.down = function(status) {
-		if (status == "pressed") {
-			rocket.move = true;
-			rocket.moveBack = true;
-			rocket.moveFwd = false;
-		} else{
-			rocket.move = false;
-			// rocket.moveBack = false;
-		};
+	rocket.down = function() {
+		rocket.accelerate = false;
+		rocket.move = false;
 	};
 
 	rocket.left = function(status) {
@@ -102,64 +119,93 @@ function loaded () {
 		};
 	};
 
-	rocket.update = function() {
+	rocket.draw = function() {
+		context.translate(rocket.x, rocket.y); /*Translate context to rocket position. ie, get it to rocket pos.*/
+		context.rotate(rocket.angle*(Math.PI/180)); /*Rotate context to angle required*/
+		
 		if (rocket.move) {
+			context.drawImage(rocket.spriteMoving, 0, 0, rocket.width, rocket.height+4); /*Draw the image onto rotated context*/
+		} else {
+			context.drawImage(rocket.spriteNormal, 0, 0, rocket.width, rocket.height); /*Draw the image onto rotated context*/
+		};
+		
+		context.rotate(rocket.angle*(Math.PI/180) * -1); /*Rotate context back to normal after drawing*/
+		context.translate(-rocket.x, -rocket.y); /*Translate context back to old position*/
+	};
+
+	rocket.update = function() {
+
+		if (rocket.move) {
+			rocket.x += Math.sin(rocket.angle*(Math.PI/180))*rocket.speed;
+			rocket.y += Math.cos(rocket.angle*(Math.PI/180))*rocket.speed*-1;
+
+			//place outside this 'if' to make it turn even when not moving
 			if (rocket.rotating) {
 				if (rocket.clkwise) {
 					rocket.angle += 5;					
 				} else {
 					rocket.angle -= 5;
 				}
-			}
-			rocket.speed = 5; /*Speed drops to 0 when nothing is done. This is to reset it to value*/
-		}
-		rocket.speed *= friction;
-
-		if (rocket.moveFwd) {
-			rocket.x += Math.sin(rocket.angle*(Math.PI/180))*rocket.speed;
-			rocket.y += Math.cos(rocket.angle*(Math.PI/180))*rocket.speed*-1;
+			};
 		} else {
-			rocket.x -= Math.sin(rocket.angle*(Math.PI/180))*rocket.speed;
-			rocket.y -= Math.cos(rocket.angle*(Math.PI/180))*rocket.speed*-1;
+			rocket.speed = 0;
+		};
+
+		if (rocket.accelerate) {
+			rocket.speed = 5; /*Speed drops to 0 when nothing is done. This is to reset it to value*/
+		} else {
+			rocket.speed *= friction;
+		};
+
+		/*Due to friction speed drops. When it goes below treshhold, move = false 
+			so that sprite image is changed*/
+		if (rocket.speed <= 0.3) {
+			rocket.move = false;
+		};
+
+		//rocket wrap around
+		if (rocket.x > global.width) {
+			rocket.x = 0;
+		} else if (rocket.x < 0) {
+			rocket.x = global.width;
+		} else if (rocket.y > global.height) {
+			rocket.y = 0;
+		} else if (rocket.y < 0) {
+			rocket.y = global.height;
 		};
 	}
 
 	function keydown (event) {
 		var key = event.keyCode;
-		var left = 37, up = 38, right = 39, down = 40;
 		
 		switch (key) {
-			case left:
+			case global.left:
 				rocket.left('pressed');
 				break;
-			case up:
+			case global.up:
 				rocket.up('pressed');
 				break;
-			case right: 
+			case global.right: 
 				rocket.right('pressed');
 				break;
-			case down:
-				rocket.down('pressed');
+			case global.down:
+				rocket.down();
 				break;
 		}
 	};
 
 	function keyup (event) {
 		var key = event.keyCode;
-		var left = 37, up = 38, right = 39, down = 40;
 		
 		switch (key) {
-			case left:
+			case global.left:
 				rocket.left('released');
 				break;
-			case up:
+			case global.up:
 				rocket.up('released');
 				break;
-			case right: 
+			case global.right: 
 				rocket.right('released');
-				break;
-			case down:
-				rocket.down('released');
 				break;
 		}
 	};
@@ -169,6 +215,7 @@ function loaded () {
 		context = canvas.getContext('2d');
 		global.width = canvas.width = window.innerWidth;
 		global.height = canvas.height = window.innerHeight;
+		global.left = 37, global.up = 38, global.right = 39, global.down = 40;
 
 		rocket.x = (rocket.width + global.width)/2;
 		rocket.y = (rocket.height + global.height)/2;
@@ -178,8 +225,8 @@ function loaded () {
 
 		for (var i = 0; i < global.meteorNumber; i++) {
 			meteor.push(new Meteor());
-			meteor[i].sprite = document.getElementById('asteroid') 
- 		}; 
+			meteor[i].sprite = document.getElementById('asteroid');
+		}; 
 		document.addEventListener('keydown', keydown);
 		document.addEventListener('keyup', keyup);
 	};
