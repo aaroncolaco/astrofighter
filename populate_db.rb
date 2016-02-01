f582b26eef0228c2d23798ebc477f826a9f75274
aarongoa <taipan93@gmail.com>
2016-02-01 21:06:06 +0530

    Added script to populate and clear DB

---
+++ populate_db.rb
@@ -0,0 +1,17 @@
+require './player.rb'
+
+no_of_players = 10
+
+# Populate db
+no_of_players.times do |var|
+	player=Player.new
+	player.email = "user#{var}@gmail.com"
+	player.name = "user#{var}"
+	player.score = rand(20)
+	player.save
+end
+
+# Clear db
+# Player.count.times do |var|
+# 	Player.last.destroy
+# end
