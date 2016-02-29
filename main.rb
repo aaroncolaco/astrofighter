226204f4b38974f3b8056ec1cfe2f6a65ca3af74
aarongoa <taipan93@gmail.com>
2016-03-01 01:58:00 +0530

    Home page done, added necessary files

--- main.rb
+++ main.rb
@@ -3,13 +3,19 @@ require 'sinatra'
 # Reload if in development mode
 require 'sinatra/reloader' if development?
 
+set :app_name, 'Astrofighter'
+
 get '/' do
-	'Home Page'
+	erb :home
+end
+
+get '/about' do
+	'About Page'
 end
 
 get '/game/?' do 	# for /game & /game/ 
 	@title = "Asteroid Game"
-	erb :game
+	erb :game, :layout => nil	# So that default layout not used
 end
 
 get '/scores' do
@@ -28,4 +34,15 @@ not_found do
 	# @title = "Not Found"
 	# erb :not_found
 	'Not found'
+end
+
+helpers do
+	def current?(path = '/')
+		(request.path == path || request.path == path+'/') ? "active" : nil
+	end
+	def set_title
+		# if not given, set to app name. That is why the OR
+		@title ||= settings.app_name
+		# Done so that we don't have to put this login in the erb file like before
+	end
 end
